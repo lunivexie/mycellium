c28d3df55c7c6b265ebe10f77ecdaee340bcd009
Lunivexie <lunivex.dev@gmail.com>
2026-04-18 15:56:21 -0400

    fix: revert base path for default url

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Cambiado de '/mycelium/' a '/' para dominio personalizado
+  base: '/mycelium/', // Cambiado de '/' a '/mycelium/' para que funcione en la URL de GitHub por defecto
 })
